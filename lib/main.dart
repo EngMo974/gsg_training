86fc300c73a97d18eadd54e1eebba28f6e3c4bc2
EngMo974 <42417845+EngMo974@users.noreply.github.com>
2020-12-01 17:40:38 +0200

    task 1

---
+++ lib/main.dart
@@ -0,0 +1,242 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile',
+      home: MyName(),
+    );
+  }
+}
+
+class MyName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                ),
+                Text(
+                  'Edit Profile',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 16,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text(
+                    'Save',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 16,
+                        color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Center(
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.7), BlendMode.darken),
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/prof.jpg'),
+                  ),
+                ),
+                child: Center(
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.camera_alt,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Divider(),
+            SizedBox(
+              height: 2,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 2, left: 16, bottom: 2),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Name',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    'Mohammed K. Khalifa',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 17,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.only(top: 2, left: 16, bottom: 2),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Address',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    '1\'st Street, Rafah, Gaza-Strip',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 17,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.only(top: 2, left: 16, bottom: 2),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Email',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    'moh.khalifa04@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 17,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.only(top: 2, left: 16, bottom: 2),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Mobile',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    '009725987654321',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 17,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.only(top: 2, left: 16, bottom: 2),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Birthdate',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 14,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        '29/April/1997',
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 17,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: Icon(
+                          Icons.calendar_today,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
